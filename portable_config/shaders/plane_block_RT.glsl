a7f81a7f52db1479a0de0b227151e615a7f99731
hooke007 <hooke007@qq.com>
2025-05-11 19:07:41 +0800

    着色器： 新增 plane_block_RT
    模拟屏蔽YUV的任意通道，默认参数为屏蔽UV通道（即仅保留Luma）；
    恰好在功能上覆盖了 gray_chroma 着色器，因此同时移除此功能重复的着色器。
    
    运行时可调节参数见 https://github.com/hooke007/MPV_lazy/discussions/410

---
+++ portable_config/shaders/plane_block_RT.glsl
@@ -0,0 +1,60 @@
+
+//!PARAM bY
+//!TYPE DEFINE
+//!MINIMUM 0
+//!MAXIMUM 1
+0
+
+//!PARAM bY_mode
+//!TYPE DEFINE
+//!MINIMUM 1
+//!MAXIMUM 3
+1
+
+//!PARAM bU
+//!TYPE DEFINE
+//!MINIMUM 0
+//!MAXIMUM 1
+1
+
+//!PARAM bV
+//!TYPE DEFINE
+//!MINIMUM 0
+//!MAXIMUM 1
+1
+
+//!HOOK LUMA
+//!BIND HOOKED
+//!DESC [plane_block_RT] Y -->> black/grey/white
+//!WHEN bY 0 >
+
+vec4 hook() {
+	vec4 color = HOOKED_texOff(0.0);
+#if (bY_mode == 1)
+	color.x = 0.0;
+#endif
+#if (bY_mode == 2)
+	color.x = 0.5;
+#endif
+#if (bY_mode == 3)
+	color.x = 1.0;
+#endif
+	return color;
+}
+
+//!HOOK CHROMA
+//!BIND HOOKED
+//!DESC [plane_block_RT] U/V -->> grey
+//!WHEN bU 0 > bV 0 > +
+
+vec4 hook() {
+	vec4 color = HOOKED_texOff(0.0);
+#if (bU == 1)
+	color.x = 0.5;
+#endif
+#if (bV == 1)
+	color.y = 0.5;
+#endif
+	return color;
+}
+
